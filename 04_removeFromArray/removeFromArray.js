a141b9912ac91932cc32baba4df048b3c3deaf07
Ryszawy <Ryszzawy@protonmail.com>
2022-10-19 21:05:41 +0200

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elements) {
+  let result = Array.from(array);
 
+  for (const elem of elements) {
+    let tempArr = new Array();
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] !== elem) {
+        tempArr.push(result[i]);
+      }
+    }
+
+    result = Array.from(tempArr);
+    tempArr = [];
+  }
+
+  return result;
 };
 
 // Do not edit below this line
