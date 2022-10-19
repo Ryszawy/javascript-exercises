a6a6524f4014657cc61ed1bb7fed347d1d5c6dab
Ryszawy <Ryszzawy@protonmail.com>
2022-10-19 21:18:44 +0200

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (firstValue, secondValue) {
+  let result = 0;
 
+  if (firstValue < 0 || secondValue < 0) return "ERROR";
+
+  if (typeof firstValue === "string" || typeof secondValue === "string")
+    return "ERROR";
+
+  if (Array.isArray(firstValue) || Array.isArray(secondValue)) return "ERROR";
+
+  if (firstValue > secondValue) {
+    let temp = firstValue;
+    firstValue = secondValue;
+    secondValue = temp;
+  }
+
+  for (let i = firstValue; i <= secondValue; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
