43391e734d89010b5bc3554a0ceaf0f8e1073fff
Ryszawy <Ryszzawy@protonmail.com>
2022-10-19 19:37:14 +0200

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (repeatString, repetitionNumber) {
+  let result = "";
 
+  if (repetitionNumber < 0) return "ERROR";
+
+  for (let i = 0; i < repetitionNumber; i++) {
+    result += repeatString;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
