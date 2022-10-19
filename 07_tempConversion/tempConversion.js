d5f715b327adcc442ca7be983244bd3657bfe7ee
Ryszawy <Ryszzawy@protonmail.com>
2022-10-19 21:36:45 +0200

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheitTemp) {
+  let celsiusTemp = ((fahrenheitTemp - 32) * 5) / 9;
+  return Math.round(celsiusTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  return Math.round(fahrenheitTemp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
