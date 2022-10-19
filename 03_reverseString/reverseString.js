6631fc5e2c1f7ce66b21a71da3562e22f9d76521
Ryszawy <Ryszzawy@protonmail.com>
2022-10-19 19:56:17 +0200

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (originalString) {
+  let result = "";
 
+  for (let i = originalString.length; i >= 0; i--) {
+    result += originalString.charAt(i);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
