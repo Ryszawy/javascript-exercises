96135abb52876a718b7b0d0619ff819c38945de2
Ryszawy <Ryszzawy@protonmail.com>
2022-10-26 19:57:10 +0200

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  word = word.toLowerCase().replace(/[^a-z]/g, "");
+  word = word.split("");
+  for (let i = 0, j = word.length - 1; ; i++, j--) {
+    if (i >= j) break;
+    if (word[i] != word[j]) return false;
+  }
 
+  return true;
 };
 
 // Do not edit below this line
