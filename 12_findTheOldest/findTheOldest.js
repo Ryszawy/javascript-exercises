2c1a72d23777c44f1dae22862b9927c69c54397b
Ryszawy <Ryszzawy@protonmail.com>
2022-10-27 20:01:11 +0200

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.sort((a, b) => {
+    let aPerson;
+    let bPerson;
+    const today = new Date();
 
+    if (typeof a.yearOfDeath === "undefined") {
+      aPerson = today.getFullYear() - a.yearOfBirth;
+    } else {
+      aPerson = a.yearOfDeath - a.yearOfBirth;
+    }
+
+    if (typeof b.yearOfDeath === "undefined") {
+        bPerson = today.getFullYear() - b.yearOfBirth;
+      } else {
+        bPerson = b.yearOfDeath - b.yearOfBirth;
+      }
+
+    return aPerson > bPerson ? -1 : 1;
+  });
+
+  return oldest[0];
 };
 
 // Do not edit below this line
